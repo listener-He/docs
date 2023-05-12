ad70ab6d2bddcb55dd17efa89e04394a00b8b754
Honesty <hehouhui@foxmail.com>
2023-05-12 15:40:24 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -11,13 +11,13 @@ export default defineUserConfig({
   // 网站语言，默认为中文
   lang: "zh-CN",
   // 网站标题
-  title: "LearnData-开源笔记",
+  title: "Honesty笔记",
   // 网站描述
   description:
-    "开源工具、效率方法、心理学探索的自我提升笔记，记录并输出一切能让自己提升的知识。",
+    "记录能提升自己的开源工具、效率技巧和心理学，输出自我提升笔记",
 
   // 网站路径默认为主域名。如果网站部署在子路径下，比如 xxx.com/yyy，那么 base 应该被设置为 "/yyy/"
-  base: "/",
+  base: "/blog-cn/",
 
   theme,
   // 是否开启页面预拉取，如果服务器宽带足够，可改为 true，会提升其他页面加载速度
@@ -38,19 +38,19 @@ export default defineUserConfig({
 
   plugins: [
     // algolia 全文搜索：没设置爬虫的话，需删除 docsearchPlugin 区块以使用节点搜索
-    docsearchPlugin({
+<!--     docsearchPlugin({
       indexName: "newzone",
       appId: "M4EXXEZIEG",
       apiKey: "fd8891a9c4cc21e0ef4f11bf44f7a11e",
-    }),
+    }), -->
     // 本地搜索，删除上方 docsearchPlugin 区块后生效
     searchProPlugin({
       // 索引全部内容
       indexContent: true,
     }),
     // 谷歌分析 ID
-    googleAnalyticsPlugin({
+<!--     googleAnalyticsPlugin({
       id: "G-RWKZTY2P9R",
-    }),
+    }), -->
   ],
 });
